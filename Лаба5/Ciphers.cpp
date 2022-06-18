e4d5e4dd873d18500399fb51f808231222f9e79a
FominDA <fomindanjj@rambler.ru>
2022-06-18 20:37:13 +0700

    морзе фикс

--- Лаба5/Ciphers.cpp
+++ Лаба5/Ciphers.cpp
@@ -84,9 +84,9 @@ string Encrypt6(string text) {
     {"7","7", "--..."},
     {"8","8", "---.."},
     {"9","9", "----."},
-    {".",".",".-.-.-"},
-    {",",",","--..--"},
-    {"!","!","-.-.--"},
+    {",",",",".-.-.-"},
+    {".",".","......"},
+    {"!","!","--..--"},
     {"?","?","..--.."},
     {"'","'",".----."},
     {"\"","\"",".-..-."},
@@ -159,9 +159,9 @@ string Decrypt6(string text) {
     {"7","7", "--..."},
     {"8","8", "---.."},
     {"9","9", "----."},
-     {".",".",".-.-.-"},
-    {",",",","--..--"},
-    {"!","!","-.-.--"},
+    {",",",",".-.-.-"},
+    {".",".","......"},
+    {"!","!","--..--"},
     {"?","?","..--.."},
     {"'","'",".----."},
     {"\"","\"",".-..-."},
@@ -283,33 +283,33 @@ string Decrypt2(string cEng) {
     return DeCrypted;
 }
 string Encrypt3(string cEng) {
-	string Crypted = cEng;  //DeCrypted Crypted
-	int i = 0; //Crypted position counter		
-	while (Crypted[i] != '\0') { //value substitution
-		if (cEng[i] >= 65 && cEng[i] <= 90) {
-			Crypted[i] = 90 - cEng[i] % 65;
-		}
-		if (cEng[i] >= 97 && cEng[i] <= 122) {
-			Crypted[i] = 122 - cEng[i] % 97;
-		}			
-		i++;		
-	}
-	return Crypted;
+    string Crypted = cEng;  //DeCrypted Crypted
+    int i = 0; //Crypted position counter		
+    while (Crypted[i] != '\0') { //value substitution
+        if (cEng[i] >= 65 && cEng[i] <= 90) {
+            Crypted[i] = 90 - cEng[i] % 65;
+        }
+        if (cEng[i] >= 97 && cEng[i] <= 122) {
+            Crypted[i] = 122 - cEng[i] % 97;
+        }
+        i++;
+    }
+    return Crypted;
 }
 string Decrypt3(string cEng) {
-	
-	string DeCrypted = cEng;  //DeCrypted Crypted
-	int i = 0; //Crypted position counter		
-	while (DeCrypted[i] != '\0') { //value substitution
-		if (cEng[i] >= 65 && cEng[i] <= 90) {
-			DeCrypted[i] = 90 - cEng[i] % 65;
-		}
-		if (cEng[i] >= 97 && cEng[i] <= 122) {
-			DeCrypted[i] = 122 - cEng[i] % 97;
-		}
-		i++;
-	}
-	return DeCrypted;
+
+    string DeCrypted = cEng;  //DeCrypted Crypted
+    int i = 0; //Crypted position counter		
+    while (DeCrypted[i] != '\0') { //value substitution
+        if (cEng[i] >= 65 && cEng[i] <= 90) {
+            DeCrypted[i] = 90 - cEng[i] % 65;
+        }
+        if (cEng[i] >= 97 && cEng[i] <= 122) {
+            DeCrypted[i] = 122 - cEng[i] % 97;
+        }
+        i++;
+    }
+    return DeCrypted;
 }
 string EncryptForKey(string cEng) {
     int key = 5;
